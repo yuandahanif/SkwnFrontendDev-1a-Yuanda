d33962b40284b5ab24c847ef67afb3b9fb6a64c6
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2023-07-26 02:07:44 +0700

    feat: :sparkles: add some animation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,40 @@
-import * as $ from 'jquery';
+/// <reference types="jquery" />
 
-console.log("Hello World from your main file!");
+// ! toggle mobile navigation menu
+$("#mobile_nav_button").on("click", (e) => {
+  $("#main_nav").toggleClass("nav-active");
+});
+
+$("#main_nav > ul > li a").on("click", (e) => {
+  $("#main_nav").toggleClass("nav-active");
+});
+
+// ! on scroll
+let itter = 0;
+$(window).on("scroll", (e) => {
+  const scroll = $(window)?.scrollTop();
+  console.log("file: index.ts:16 ~ $ ~ scroll:", scroll);
+
+  if (scroll && scroll > 100) {
+    $("header").addClass("shadow");
+  } else {
+    $("header").removeClass("shadow");
+  }
+
+  if (itter == 0 && scroll && scroll > 600) {
+    const intervalId = setInterval(() => {
+      itter++;
+      $("#partner_count").text(`${itter}+`);
+
+      if (itter > 20) {
+        clearInterval(intervalId);
+      }
+    }, 100);
+  }
+});
+
+// ! recommended product on click
+$(".product__coursel__item").on("click", (e) => {
+  $(".product__coursel__item").removeClass("product__coursel__item--active");
+  e.currentTarget.classList.add("product__coursel__item--active");
+});
